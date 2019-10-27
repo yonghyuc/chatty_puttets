a530b6f85de9547a93cee894b3ddc27454290ac8
yonghyuc <yonghyuc@usc.edu>
2019-10-26 23:26:11 -0700

    chagne port to 80

--- app.py
+++ app.py
@@ -4,4 +4,4 @@ from app import app, socketio
 
 if __name__ == '__main__':
     print ("start!")
-    socketio.run(app, debug=True, host="0.0.0.0", port=5000)
+    socketio.run(app, host="0.0.0.0", port=80, debug=True)
